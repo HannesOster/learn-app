bfbbae8afcaeca71a3bc80ceb786a7ac9bc53c4a
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-10-04 15:34:45 +0200

    add card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, Button } from "react-bootstrap";
+import "./App.css";
+
+const data = [
+  { question: "Frage 1", answer: "Antwort 1" },
+  { question: "Frage 2", answer: "Antwort 2" },
+  { question: "Frage 3", answer: "Antwort 3" },
+];
 
 function App() {
+  const [showAnswer, setShowAnswer] = useState(Array(data.length).fill(false));
+
+  const toggleAnswer = (index) => {
+    const newShowAnswer = [...showAnswer];
+    newShowAnswer[index] = !newShowAnswer[index];
+    setShowAnswer(newShowAnswer);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {data.map((item, index) => (
+        <Card key={index} className="card">
+          <Card.Body>
+            <Card.Title>{item.question}</Card.Title>
+            {showAnswer[index] && <Card.Text>{item.answer}</Card.Text>}
+            <Button variant="primary" onClick={() => toggleAnswer(index)}>
+              {showAnswer[index] ? "Verbergen" : "Antwort anzeigen"}
+            </Button>
+          </Card.Body>
+        </Card>
+      ))}
     </div>
   );
 }
